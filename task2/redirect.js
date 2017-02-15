84dd8cd0df557962938481c6a16d7ca0616a48df
jnshu2040 <liuqiyong0319@live.com>
2017-02-16 01:51:14 +0800

    task2/000

---
+++ task2/redirect.js
@@ -0,0 +1,32 @@
+/**
+ * Created by 1 on 2017/2/15.
+ */
+
+
+
+
+function beginPage() {
+    window.location.href="kaishiye.html"
+}
+function selectPeoplePage(){
+    window.location.href="xuanrenye.html"
+}
+
+
+function beginPageTime() {
+    setTimeout(beginPage,3000)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
